fd187fe1d8cfa4c59b5cafe94467f24fb21f7957
Ibrahim FriendshipWala <ibrahimfriendshipwala@gmail.com>
2025-08-19 17:46:17 +0500

    Create reschedule.php

---
+++ reschedule.php
@@ -0,0 +1,4 @@
+<?php
+// For simplicity this page is left as a placeholder. You can wire similar logic as create_booking
+echo "Reschedule flow can be implemented: show available slots, create new booking, mark old as rescheduled and notify visitor.";
+?>
